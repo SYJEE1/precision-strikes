b0ba2ba1346f1ac1edfa780a52e89076bcfb9b91
SYJEE1 <166915640+SYJEE1@users.noreply.github.com>
2024-04-16 15:33:29 +0800

    ducking problem

--- Scene/warrior.gd
+++ Scene/warrior.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+const GRAVITY : int = 4200
+const JUMP_SPEED : int = -1800
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		$RunCol.disabled = false
+		if Input.is_action_pressed("ui_up"):
+			velocity.y = JUMP_SPEED
+			#sound
+		elif Input.is_action_just_pressed("crouch"):
+			$AnimatedSprite2D.play("duck")
+			$RunCol.disabled = true
+		else:
+			$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+		
+	move_and_slide()
