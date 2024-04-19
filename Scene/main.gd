e5fc12eefe37c8fadf3deea885c156ac2e0aafcb
jnwync <jonwayne.cabusbusan@gmail.com>
2024-04-19 18:44:20 +0800

    speed increments upon pressing z

--- Scene/main.gd
+++ Scene/main.gd
@@ -1,26 +1,25 @@
 extends Node
 
-
 const WARRIOR_START_POS := Vector2i(150, 485)
-const CAM_START_POS 	:= Vector2i(576, 324)
+const CAM_START_POS := Vector2i(576, 324)
 
-var speed : float
-const  START_SPEED : float = 6.0
-const MAX_SPEED : int = 25
-var screen_size : Vector2i
+var speed: float = START_SPEED
+const START_SPEED: float = 6.0
+const MAX_SPEED: float = 24.0
+var screen_size: Vector2i
 
 func _ready():
-	screen_size = get_window().size
+	screen_size = get_viewport().size
 	new_game()
 
 func new_game():
 	$Warrior.position = WARRIOR_START_POS
-	$Warrior.velocity = Vector2i(0,0)
+	$Warrior.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
-	$Ground.position = Vector2i(0,0)
-	
-func _process(delta):
+	$Ground.position = Vector2i(0, 0)
 	speed = START_SPEED
+
+func _process(delta):
 	if Input.is_action_pressed("forward"):
 		$Warrior.position.x += speed
 		$Camera2D.position.x += speed
@@ -29,3 +28,11 @@ func _process(delta):
 		$Camera2D.position.x -= speed
 	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 		$Ground.position.x += screen_size.x
+
+func _input(event):
+	if event.is_action_pressed("faster"):
+		increase_speed()
+
+func increase_speed():
+	speed += 2.0
+	speed = min(speed, MAX_SPEED)
