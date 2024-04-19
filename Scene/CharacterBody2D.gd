0d3b46cf3c8b794cf05bece4f9f2edc0c2f6e89a
SYJEE1 <166915640+SYJEE1@users.noreply.github.com>
2024-04-19 17:02:00 +0800

    Slime

--- Scene/CharacterBody2D.gd
+++ Scene/CharacterBody2D.gd
@@ -8,18 +8,15 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if  is_on_floor():
 		$RunCol.disabled = false
-		if Input.is_action_pressed("jump"):
-			velocity.y = JUMP_SPEED
-		elif Input.is_action_just_pressed("duck"):
-			$AnimatedSprite2D.play("duck")
-			$RunCol.disabled = true
-		elif Input.is_action_just_pressed("attack"):
+		$AttCol.disabled = true
+		if Input.is_action_pressed("attack"):
 			$AnimatedSprite2D.play("attack")
-			$RunCol.disabled = true
-		else:
+			$AttCol.disabled = false
+			velocity.x = 0
+		elif Input.is_action_pressed("forward"):
 			$AnimatedSprite2D.play("run")
+		else:
+			$AnimatedSprite2D.play("idle")
 	
-	else:
-		$AnimatedSprite2D.play("jump")
-		
 	move_and_slide()
+
--- Scene/main.gd
+++ Scene/main.gd
@@ -5,7 +5,7 @@ const WARRIOR_START_POS := Vector2i(150, 485)
 const CAM_START_POS 	:= Vector2i(576, 324)
 
 var speed : float
-const  START_SPEED : float = 10.0
+const  START_SPEED : float = 6.0
 const MAX_SPEED : int = 25
 var screen_size : Vector2i
 
@@ -21,8 +21,11 @@ func new_game():
 	
 func _process(delta):
 	speed = START_SPEED
-	$Warrior.position.x += speed
-	$Camera2D.position.x += speed
-	
+	if Input.is_action_pressed("forward"):
+		$Warrior.position.x += speed
+		$Camera2D.position.x += speed
+	if Input.is_action_pressed("attack"):
+		$Warrior.position.x -= speed
+		$Camera2D.position.x -= speed
 	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 		$Ground.position.x += screen_size.x
